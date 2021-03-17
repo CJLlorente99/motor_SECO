becd879587a675b6e18d35e1ebf2cc079d1c7106
Char Llorente Cortijo <cj.llorente@alumnos.upm.es>
2021-03-17 23:48:31 +0100

    MATLAB files and python script for getting the required by MATLAB files from the CSV data obtained from serial. Data obtained from nuevosdatos.txt and generated by this script has also been included

---
+++ python/fromDatatoFile.py
@@ -0,0 +1,33 @@
+import argparse
+import pandas as pd
+
+parser = argparse.ArgumentParser(description='From CSV data taken from serial to files for each voltage')
+parser.add_argument('inputFile', type=str, help='Path to input CSV file (the one generated from serial)')
+parser.add_argument('outputFolder', type=str, help='Folder where output files will be placed (CARE! use / or \\ at the end)')
+
+args = parser.parse_args()
+
+df = pd.read_csv(args.inputFile)
+
+voltageValues = df['VOLTAGE'].unique()
+meanValues = pd.DataFrame(columns=voltageValues, index=range(1201))
+
+for voltage in voltageValues:
+    print('\nDealing with voltage ' + str(voltage))
+    print('This voltage has ' + str(df[(df['VOLTAGE'] == voltage) & (df[' INDEX'] == 0)][' PULSES'].count()) + ' repetitions')
+    for i in range(1201):
+        meanValues[voltage][i] = df[(df['VOLTAGE'] == voltage) & (df[' INDEX'] == i)][' PULSES'].mean()
+    print('Dealt with voltage ' + str(voltage) + '\n')
+
+for voltage in voltageValues:
+    if(voltage % 1 == 0):
+        fileName ='MotorDCPololu.0ms600ms600ms_T1ms_ST.mean.' + str(int(voltage)) + 'V.polo'
+    else:
+        fileName ='MotorDCPololu.0ms600ms600ms_T1ms_ST.mean.' + str(voltage) + 'V.polo'
+
+    print('Starting to write ' + fileName) 
+    with open(args.outputFolder + fileName, 'w') as f:
+        for i in range(1201):
+            f.write(str(i) + ' ' + str(meanValues[voltage][i]) + "\n")
+    print('Finished writing ' + fileName + '\n')
+
