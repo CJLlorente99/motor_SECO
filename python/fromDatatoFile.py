7221122042f0251da2ede1db8252e08cebe0a415
Carlos Llorente <cj.llorente@alumnos.upm.es>
2021-03-27 00:38:01 +0100

    Python script for taking data and plotting controller tests

--- python/fromDatatoFile.py
+++ python/fromDatatoFile.py
@@ -1,5 +1,8 @@
 import argparse
 import pandas as pd
+import plotly.express as px
+import math
+import os
 
 parser = argparse.ArgumentParser(description='From CSV data taken from serial to files for each voltage')
 parser.add_argument('inputFile', type=str, help='Path to input CSV file (the one generated from serial)')
@@ -7,27 +10,50 @@ parser.add_argument('outputFolder', type=str, help='Folder where output files wi
 
 args = parser.parse_args()
 
-df = pd.read_csv(args.inputFile)
+if not os.path.exists(os.path.dirname(args.outputFolder)):
+    try:
+        os.makedirs(os.path.dirname(args.outputFolder))
+    except:
+        exit()
 
-voltageValues = df['VOLTAGE'].unique()
-meanValues = pd.DataFrame(columns=voltageValues, index=range(1201))
+if not os.path.exists(os.path.dirname(args.outputFolder + 'data/')):
+    try:
+        os.makedirs(os.path.dirname(args.outputFolder + 'data/'))
+    except:
+        exit()
 
-for voltage in voltageValues:
-    print('\nDealing with voltage ' + str(voltage))
-    print('This voltage has ' + str(df[(df['VOLTAGE'] == voltage) & (df[' INDEX'] == 0)][' PULSES'].count()) + ' repetitions')
-    for i in range(1201):
-        meanValues[voltage][i] = df[(df['VOLTAGE'] == voltage) & (df[' INDEX'] == i)][' PULSES'].mean()
-    print('Dealt with voltage ' + str(voltage) + '\n')
+df = pd.read_csv(args.inputFile, skipinitialspace=True, skip_blank_lines=True)
 
-for voltage in voltageValues:
-    if(voltage % 1 == 0):
-        fileName ='trap' + str(int(voltage)) + 'V_0ms600ms600ms_T1ms_ST.mean.'
-    else:
-        fileName ='trap' + str(voltage) + 'V_0ms600ms600ms_T1ms_ST.mean.'
+uniqueExperiments = df.groupby(['FINALRAD','CONTROLLER','KP','TAUD','TAUI']).size().reset_index().drop(0,axis=1)
+uniqueExperiments = df[['FINALRAD','CONTROLLER','KP','TAUD','TAUI']].drop_duplicates().dropna().reset_index(drop=True)
 
-    print('Starting to write ' + fileName) 
-    with open(args.outputFolder + fileName, 'w') as f:
-        for i in range(1201):
-            f.write(str(i) + ' ' + str(meanValues[voltage][i]) + "\n")
-    print('Finished writing ' + fileName + '\n')
+# Create tags
+tags = []
+print('Number of unique experiments is : ' + str(uniqueExperiments.shape[0]))
+for i in range(uniqueExperiments.shape[0]):
+    tags.append('exp_' + str(uniqueExperiments['FINALRAD'][i]) + '_' + str(uniqueExperiments['CONTROLLER'][i]) + '_' + str(uniqueExperiments['KP'][i]) + '_' + str(uniqueExperiments['TAUD'][i]) + '_' + str(uniqueExperiments['TAUI'][i]))
+uniqueExperiments['TAG'] = tags
 
+# Summary dile
+with open(args.outputFolder + 'summary.txt','w') as file:
+    file.write('Los experimentos presentes y sus nombres son\n')
+    for i in range(uniqueExperiments.shape[0]):
+        file.write('FinalRad = ' + str(uniqueExperiments['FINALRAD'][i]) + ', ' + 'Controller = ' + str(uniqueExperiments['CONTROLLER'][i]) + ', ' \
+        + ', ' + 'Kp = ' + str(uniqueExperiments['KP'][i]) + ', ' + 'TauD = ' + str(uniqueExperiments['TAUD'][i]) + ', ' + 'TauI = ' + str(uniqueExperiments['TAUI'][i]) + ', ' \
+        + 'nombre del fichero = ' + args.outputFolder + 'data/' + uniqueExperiments['TAG'][i] + '.txt\n')
+
+# For each unique experiment create file with data and plot
+for i in range(uniqueExperiments.shape[0]):
+    data = df.loc[(df['FINALRAD'] == uniqueExperiments['FINALRAD'][i]) & (df['CONTROLLER'] == uniqueExperiments['CONTROLLER'][i]) & (df['KP'] == uniqueExperiments['KP'][i]) & (df['TAUD'] == uniqueExperiments['TAUD'][i]) & (df['TAUI'] == uniqueExperiments['TAUI'][i])].reset_index(drop=True)
+    with open(args.outputFolder + 'data/' + uniqueExperiments['TAG'][i] + '.txt','w') as file:
+        file.write('TIME,PULSES,CONTROLLERVOLTAGE,CONTROLLER,KP,TAUD,TAUI \n')
+        for j in range(data.shape[0]):
+            file.write(str(data['TIME'][j]) + ',' + str(data['PULSES'][j]) + ',' + str(data['CONTROLLERVOLTAGE'][j]) + ',' + str(data['CONTROLLER'][j]) + ',' + str(data['KP'][j]) + ',' + str(data['TAUD'][j]) + ',' + str(data['TAUI'][j]) + '\n')
+
+    fig = px.scatter(x=data['TIME'], y=(data['PULSES']*2*math.pi/48/75), title=uniqueExperiments['TAG'][i], labels=dict(x='Time [ms]', y='Position [rad]'))
+    fig.add_hline(y=uniqueExperiments['FINALRAD'][i], line_color='red', line_width=1, line_dash='dash')
+
+    fig.write_html(args.outputFolder + 'data/' + uniqueExperiments['TAG'].loc[i] + '.html')    
+    print('Experiment number : ' + str(i+1) + ' processed')
+
+print("Execution finished")
